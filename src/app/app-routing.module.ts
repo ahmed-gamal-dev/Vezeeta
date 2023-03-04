bc2ccfb9fcf86ae715ed7cbb14f8e8632697091d
Amany Asad AbdElsamea <60522311+amanyasad@users.noreply.github.com>
2023-03-04 04:52:20 -0800

    amany
    header, footer , home and handle routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ContactUsComponent } from './components/contactUs/contactUs.component';
+import { PrivacyPolicyComponent } from './components/privacyPolicy/privacyPolicy.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'',redirectTo:'/Home',pathMatch:'full'},
+  {path:'Home',component: HomeComponent},
+  {path: 'ContactUs',component: ContactUsComponent },
+  {path: 'PrivatePolicy',component: PrivacyPolicyComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

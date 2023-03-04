fedec2d835113afdec6a83bd93ee0fb309fd0d43
fox <fox@gmail.com>
2023-03-04 14:08:12 +0200

    add sign-in and sign-up doctor files

---
+++ src/app/components/sign-in-doctor/sign-in-doctor.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-in-doctor',
+  templateUrl: './sign-in-doctor.component.html',
+  styleUrls: ['./sign-in-doctor.component.scss']
+})
+export class SignInDoctorComponent {
+
+}
